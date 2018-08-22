8c471522ed0d3809117e82144356a527295bab85
jrkerr2 <johnrkerr@hotmail.com>
2018-08-22 07:32:17 -0400

    partially working omdb.js to console.log

--- code/omdb.js
+++ code/omdb.js
@@ -9,6 +9,21 @@ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tril
 
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log(body);
+    for (var key in JSON.parse(body)) {
+      if (key == "Title" || key == "Year" || key == "imdbRating" || key == "Ratings[1]" 
+          || key == "Country" || key == "Language" || key == "Plot" || key == "Actors") {
+        console.log(key,": " + JSON.parse(body)[key]);
+        // console.log(JSON.parse(body)[key]);
+      }
+        
+    
+    };
+    // console.log(JSON.parse(body).Title);
+    // console.log(JSON.parse(body).Year);
+    // console.log(JSON.parse(body).imdbRating);
+    console.log(JSON.parse(body).Ratings[1]);
+    // console.log(JSON.parse(body).Title);
+    
+    
   }
 });
